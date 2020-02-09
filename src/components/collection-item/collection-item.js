94a74bd5edb8060a0d16686fd7ad844738386157
Dave Beidle <dave@skywalker.local>
2020-02-09 14:04:59 -0600

    Bug fixed on collection-items.js added images to the shop as well as styiling.

--- src/components/collection-item/collection-item.js
+++ src/components/collection-item/collection-item.js
@@ -2,16 +2,15 @@ import React from 'react';
 
 import './collection-item.scss';
 
-const CollectionItem = (id, name, price, imageUrl) => (
+const CollectionItem = ({id, name, price, imageUrl}) => (
     <div className="collection-item">
-        <div className="image" style={{bacgroundImage: `url(${imageUrl})`}}>&emsp;</div>
+        <div className="image" style={{backgroundImage: `url(${imageUrl})`}}>&emsp;</div>
+        {console.log(imageUrl)}
         <div className="collection-footer">
         <span className="name">{name}</span>
         <span className="price">{price}</span>
         </div>
-        
-
-    </div>
+   </div>
 );
 
 export default CollectionItem;
--- src/components/collection-preview/collection-preview.js
+++ src/components/collection-preview/collection-preview.js
@@ -11,7 +11,7 @@ const CollectionPreview = ({title, items}) => (
             {items
                 .filter((item, idx) => idx < 4 )
                 .map(({id, ...otherItemProps })=>(
-                <CollectionItem key={id} {...otherItemProps} ></CollectionItem>
+                <CollectionItem key={id} {...otherItemProps} />
             ))}
         </div>
     </div>

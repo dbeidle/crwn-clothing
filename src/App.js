fc0979fff951094d7b61ce5d81b5466e4a7911d6
Dave Beidle <dave@skywalker.local>
2020-02-09 14:32:19 -0600

    Added Header component and associated styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>{/*This contains the routing for the different pages and the back-button */}
         <Route exact path="/" component={HomePage} /> 
         <Route exact path="/shop" component={ShopPage} />

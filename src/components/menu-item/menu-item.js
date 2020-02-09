550a5cde28670a2045ec4c0796d274d8c0d88514
Dave Beidle <dave@skywalker.local>
2020-02-08 19:48:26 -0600

    Componentized the homepage and Stylized the Menu Items.

---
+++ src/components/menu-item/menu-item.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './menu-item.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+
+    <div className={`${size} menu-item`}>
+        <div className="background-image" style={{
+            backgroundImage: `url(${imageUrl})` }}>&emsp;</div>
+            <div className="content">
+            <div className="title">{title.toUpperCase()}</div>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+    
+);
+
+export default MenuItem;
---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../../components/directory/directory';
+
+import './homepage.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+      <h1>Welcome to the Store!</h1>
+      <Directory /> {/*This calls the directory component to render the menu that you can choose from */}
+    </div>
+  );
+  
+  export default HomePage;

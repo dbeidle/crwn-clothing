1d8385b6ea00dc03875ef4d42300b7bd139fbbb0
Dave Beidle <dave@skywalker.local>
2020-02-24 12:10:00 -0600

    Introduced redux-saga into project. Refactored store, shop, and shop-actions components to utilize redux-saga instead of thunk.

--- src/redux/shop/shop-actions.js
+++ src/redux/shop/shop-actions.js
@@ -1,27 +1,31 @@
 import ShopActionTypes from "./shop-type";
-import {
-  firestore,
-  convertCollectionsSnapshotToMap
-} from "../../firebase/firebase.utils";
+// import {
+//   firestore,
+//   convertCollectionsSnapshotToMap
+// } from "../../firebase/firebase.utils";
 
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START
 });
 
-export const fetchCollectionsStartAsync = () => {
-  return dispatch => {
-    const collectionRef = firestore.collection("collections");
-    dispatch(fetchCollectionsStart());
+// This was used with the redux-thunk add-in. It is no longer needed since moving to redux-sagas
+// Use with the commented import above. also needed is thunk in middlewares in store and change
+// change fetchCollectionStart in shop.js to fetchCollectionStartAsync
+//
+// export const fetchCollectionsStartAsync = () => {
+//   return dispatch => {
+//     const collectionRef = firestore.collection("collections");
+//     dispatch(fetchCollectionsStart());
 
-    collectionRef
-      .get()
-      .then(snapshot => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        dispatch(fetchCollectionsSuccess(collectionsMap));
-      })
-      .catch(error => dispatch(fetchCollectionsFailure(error.message)));
-  };
-};
+//     collectionRef
+//       .get()
+//       .then(snapshot => {
+//         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+//         dispatch(fetchCollectionsSuccess(collectionsMap));
+//       })
+//       .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+//   };
+// };
 
 export const fetchCollectionsSuccess = collectionsMap => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,

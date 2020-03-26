15b38ce576e43d2bcab1b4f75955c70008469f0e
Dave Beidle <dave@skywalker.local>
2020-03-26 11:54:41 -0500

    Added Cart Firebase actions to Redux.

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -35,6 +35,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: []
       };
+
+    case CartActionTypes.SET_CART_FROM_FIREBASE:
+      return {
+        ...state,
+        cartItems: action.payload
+      };
     default:
       return state;
   }
